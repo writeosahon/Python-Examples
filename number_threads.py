ed6d154354f9c99686768448c52af88dddd1c749
writeosahon <wrwiteosahon@yahoo.co.uk>
2019-09-29 20:48:10 +0100

    commit

--- number_threads.py
+++ number_threads.py
@@ -16,23 +16,37 @@ import time
 import os
 from mythreadspackage import threading_with_condition_lock
 
+# define GetNumberThread class
 class GetNumberThread(threading.Thread):
+    """ class is a Thread class used to get a number from the IntNumberProvider class.
+    """
+
     def __init__(self, number_provider_class, **kwargs):
+        """ constructor method for the  class """
+
+        # call the super class i.e. Thread constructor method
         super().__init__(**kwargs)
-        self.__numberprovider = number_provider_class
+        self.__numberprovider = number_provider_class # set the IntNumberProvider for this instance
     # end of method
     
-    def run(self):
-        while True:
-            with threading_with_condition_lock.IntNumberProvider._classlock:
+    # run method where the thread executes
+    def run(self): 
+        while True: # create an infinite loop
+            # lock/synchronise access to the IntNumberProvider class using its provider 'lock'
+            with threading_with_condition_lock.IntNumberProvider._classlock: 
+                
+                # wait for a number to be available  
                 threading_with_condition_lock.IntNumberProvider._classlock.\
                     wait_for(self.__numberprovider.is_number_available)
+                # once a number is available continue the thread and get the number
                 print("Number Gotten From Within {}: {}".\
                     format(threading.current_thread().name, self.__numberprovider.getnumber()))
+            # let thread sleep for 3 seconds before continuing the loop        
             time.sleep(3)
     # end of method
 # end of class
 
+# define PutNumberThread class
 class PutNumberThread(threading.Thread):
     def __init__(self, number_provider_class, **kwargs):
         super().__init__(**kwargs)
